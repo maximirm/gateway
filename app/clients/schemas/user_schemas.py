e29fcb7181d4535a149e91174bd4abc820966c8f
maxim.irmscher <maxim.irmscher@idealo.de>
2024-01-15 12:33:54 +0100

    change schema

--- app/clients/schemas/user_schemas.py
+++ app/clients/schemas/user_schemas.py
@@ -4,9 +4,10 @@ from pydantic import BaseModel, UUID4
 
 
 class UserResponse(BaseModel):
+    id: UUID4
+    name: str
     role: str
     token: Optional[str]
-    id: UUID4
 
     class Config:
         from_attributes = True
