2058e305d6dc6fae49962e747e638284e45acb90
maxim.irmscher <maxim.irmscher@idealo.de>
2024-01-16 11:50:15 +0100

    allow cors

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 
 from app.api import analysis_controller, user_controller, survey_controller
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],  # Erlaubt alle Methoden (GET, POST, etc.)
+    allow_headers=["*"],  # Erlaubt alle Header
+)
 
 app.include_router(analysis_controller.router, tags=["Analysis"])
 app.include_router(user_controller.router, tags=["Users"])
