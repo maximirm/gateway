08b1099601f450827e2920c49f15321285fb7139
maxim.irmscher <maxim.irmscher@idealo.de>
2024-01-24 15:21:29 +0100

    remove unused method

--- app/api/analysis_controller.py
+++ app/api/analysis_controller.py
@@ -3,9 +3,8 @@ from uuid import UUID
 from fastapi import APIRouter, Request
 
 from app.clients import survey_service_client, analysis_service_client
-from app.clients.schemas.survey_schemas import AnalyzedQuestion, AnalyzedSurvey
+from app.clients.schemas.survey_schemas import AnalyzedQuestion
 from app.decorator.has_role import has_role
-
 
 router = APIRouter()
 
@@ -16,14 +15,3 @@ async def analyze_question(request: Request, question_id: UUID):
     question = await survey_service_client.fetch_question(question_id)
     analyzed_question = await analysis_service_client.fetch_analyzed_question(question)
     return analyzed_question
-
-
-@router.get("/analyze/survey/{survey_id}/", response_model=AnalyzedSurvey)
-@has_role(["editor"])
-async def analyze_survey(request: Request, survey_id: UUID):
-    survey = await survey_service_client.fetch_survey(survey_id)
-    analyzed_survey = await analysis_service_client.fetch_analyzed_survey(survey)
-    return analyzed_survey
-
-
-
--- app/clients/analysis_service_client.py
+++ app/clients/analysis_service_client.py
@@ -3,7 +3,7 @@ import json
 import httpx
 from fastapi import HTTPException
 
-from app.clients.schemas.survey_schemas import AnalyzedQuestion, Question, Survey, AnalyzedSurvey
+from app.clients.schemas.survey_schemas import AnalyzedQuestion, Question
 from app.clients.util import custom_serializer
 
 
@@ -18,19 +18,3 @@ async def fetch_analyzed_question(question: Question) -> AnalyzedQuestion:
 
     analyzed_question_data = response.json()
     return AnalyzedQuestion(**analyzed_question_data)
-
-
-async def fetch_analyzed_survey(survey: Survey) -> AnalyzedSurvey:
-    url = f"http://localhost:8001/analyze/survey/"
-    async with httpx.AsyncClient() as client:
-        json_question = json.dumps(dict(survey), default=custom_serializer)
-        response = await client.post(url, data=json_question)
-
-    if response.status_code != 200:
-        raise HTTPException(status_code=response.status_code, detail=json.loads(response.text)['detail'])
-
-    analyzed_survey_data = response.json()
-    return AnalyzedSurvey(**analyzed_survey_data)
-
-
-
